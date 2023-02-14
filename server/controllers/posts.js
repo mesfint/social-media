0af7cab1eaa952a61c1bdd9c424584aa071923ba
Mesfin Tegegne <mesfin_meng@yahoo.com>
2023-02-14 21:15:35 +0000

    user post and like has implemented

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -12,3 +12,5 @@ router.get("/:userId/posts", verifyToken, getUserPosts);
 //update
 
 router.patch("/:id/like", verifyToken, likePost)
+
+export default router;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,17 +1,1 @@
-import express from "express"
-import {geetFeedPosts, getUserPosts,likePosts} from "../controllers/posts.js"
-import {verifyToken} from "../middleware/auth.js"
-
-const router = express.Router();
-
-//Read
-
-router.get("/", verifyToken, getFeedPosts);
-router.get("/:userId/posts", verifyToken, getFUserPosts);
-
-//Update
-router.patch("/:id/like", verifyToken, likePost );
-
-
-export default router;
-
+import Post from "../models/Post.js"
